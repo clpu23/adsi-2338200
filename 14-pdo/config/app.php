17e16a0598b8c1a7da1125a45074329ac3fcf86b
clpu23 <cristian.lopez85@misena.edu.co>
2022-09-09 11:45:36 -0500

    pod

--- 14-pdo/config/app.php
+++ 14-pdo/config/app.php
@@ -1,15 +1,17 @@
 <?php
-    // config sessions
+    // Config Sessions
     session_start();
-    
-   // config URL site
-    $base_url = 'http://localhost:8000';
-    $public = $base_url . '/public/';
-    $css    = $public . '/css';
-    $js     = $public . '/js';
 
-   //config data base
-   $host = 'localhost';
-   $user = 'root';
-   $passwd = '';
-   $name_db = 'adsi2338200';
+    // Config URL Site 
+    $base_url = 'http://localhost:8000';
+    $public   = $base_url . '/public';
+    $css      = $public . '/css';
+    $js       = $public . '/js';
+
+    // Config Data Base
+    $host    = '127.0.0.1';
+    $user    = 'root';
+    $passwd  = '';
+    $name_db = 'adsi2338200';
+
+
--- 14-pdo/index.php
+++ 14-pdo/index.php
@@ -1,4 +1,5 @@
-<?php require 'config/app.php'>;
+<?php require 'config/app.php' ?>
+<?php include('config/database.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,31 +10,78 @@
     <link rel="stylesheet" href="public/css/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="public/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/fontawesome.min.css">
+    <link rel="stylesheet" href="public/css/fonts.css">
 </head>
 <body>
     <main class="container">
         <section class="row">
-            <div class="col-8 offset-2">
+            <div class="col-md-8 offset-md-2 my-5">
                 <h1 class="text-center">
-                    <i class="fa-solid fa-user-doctor"></i>
+                    <i class="fa fa-dragon"></i>
                     Web App Pokemons
                 </h1>
                 <hr>
-                    <?php require 'config/app.php'>;
+                <a href="" class = "btn btn-lg btn-outline-success">
+                    <i class = "fa fa-plus"></i>
+                        add pokemon
+                </a>
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover mt-5">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th class="d-none d-sm-table-cell">TYPE</th>
+                                <th>IMAGE</th>
+                                <th class="d-none d-sm-table-cell">TRAINER</th>
+                                <th>ACTIONS</th>
+                            </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+                                <?php $pokemons = listAllPokemons($conx) ?>
+                                <?php foreach($pokemons as $pokemon): ?>
+                                    <tr>
+                                       <td><?php echo $pokemon['id'] ?></td>
+                                       <td><?php echo $pokemon['name'] ?></td> 
+                                       <td class="d-none d-sm-table-cell"><?php echo $pokemon['type'] ?></td> 
+                                       <td>
+                                        <img src="<?php echo $pokemon['image']?>" width="40px">
+                                       </td>
+                                       <td class="d-none d-sm-table-cell"><?php echo $pokemon['nametrainer'] ?></td>
+                                       <td>
+                                       <div class="dropdown">
+                                            <button class="btn btn-dark dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                                            <i class="fa-solid fa-list"></i>
+                                            </button>
+                                            <ul class="dropdown-menu dropdown-menu-dark">
+                                                <li><a class="dropdown-item" href="#"> <i class="fa fa-search"></i> Show</a></li>
+                                                <li><a class="dropdown-item" href="#"><i class="fa fa-pen"></i>Edit</a></li>
+                                                <li><a class="dropdown-item bg-danger" href="#"><i class="fa fa-trash"></i>Delete</a></li>
+                                            </ul>
+                                            </div>
+                                       </td>  
+                                    </tr>
+                                <?php endforeach?>
+                            </tbody>
+                    </table>
+                </div>
+                
             </div>
         </section>
+        <?php $conx=null; ?>
     </main>
     <script src="public/js/jquery-3.6.0.min.js"></script>
     <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/bootstrap.bundle.min.js">
     <script src="public/js/sweetalert2.js"></script>
     <script>
         $(document).ready(function () {
-            swal.fire(
-                'Good job!',
-                'Everything is OK!',
-                'success'
-            )
-        });
+            // Swal.fire(
+            //     'Good job!',
+            //     'Everything is OK!',
+            //     'success'
+            // )
+        })
     </script>
 </body>
 </html>
