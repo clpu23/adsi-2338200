489aa6f95b6c90d1b2ef566b822b5659c38dfb35
nanita1215 <ana.jurado2@misena.edu.co>
2022-10-25 11:54:46 -0500

    oop

---
+++ 16-oop/public/00-template.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+    <script src="../public/js/tailwind-3.2.1.js"></script>
+</head>
+<body class="bg-gradient-to-t
+             from-sky-900
+             to-sky-500
+             min-h-screen">
+    
+    <main class="m-10
+                mx-auto
+                
+                p-5
+              border-sky-300
+              bg-white/5
+                rounded ">
+     <h1 class="m4 
+                text-center
+                text-4xl
+              text-white
+                text opacity-80
+                font-medium">
+        <a href="index.php" class="float-left
+                                   transition
+                                 hover:text-white
+                                   hover:-translate-x-1">
+                                   
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z" />
+            </svg>
+
+
+        </a>
+            Template
+        </h1>
+        <section class="bg-black/10
+                        text-white
+                          text-opacity-80
+                          p-5
+                          mt-10
+                          min-h-[400px]
+                          rounded">
+        
+        
+        </section>
+       
+
+    </main>
+    
+</body>
---
+++ 16-oop/public/index.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <script src="public/js/tailwind-3.2.1.js"></script>
+</head>
+<body class="bg-gradient-to-t
+            from-black
+            to-indigo-500
+            min-h-screen">
+
+    <main class="m-10
+                 mx-auto
+                 max-w-lg 
+                 p-5 
+                 border-2 
+                 border-indigo-400 
+                 bg-white/5 
+                 rounded">
+
+        <h1 class=" m-5 
+                    text-center 
+                    text-4xl text-white 
+                    text-opacity-50 
+                    font-medium ">
+           
+           
+            OOP (Object Orient Programming)
+        </h1>
+        <section class="text-white 
+                        text-opacity-50 
+                        p-5 
+                        max-h-[700px]
+                        mt-10
+                        overflow-y-auto
+                        rounded">
+
+                    <ol class=" flex 
+                                flex-col 
+                                justify-center 
+                                items-center 
+                                gap-1">
+
+                        <li class="text-xl 
+                                 text-white/75 ">
+
+                            <a href="01-class.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 
+                                                p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded 
+                                                hover:bg-opacity-50
+                                                hover:scale-105">
+
+                                                01 - Class
+                            </a>
+                        </li>
+                        <li>
+                            <a href="02-construct.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+
+                                                02 - Construct
+                            </a>
+                        </li>
+                        <li>
+                            <a href="03-private.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                03 - Private
+                            </a>
+                        </li>
+                        <li>
+                            <a href="04-collaboration.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                04 - Colaboration
+                            </a>
+                        </li>
+                        <li>
+                            <a href="05-parameters.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                05 - Parameters
+                            </a>
+                        </li>
+                        <li>
+                            <a href="06-extends.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                06 - Extends
+                            </a>
+                        </li>
+                        <li>
+                            <a href="07-overwrite-method.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                07 - Overwrite Method
+                            </a>
+                        </li>
+                        <li>
+                            <a href="08-overwrite-construct.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                08 - Overwrite Construct
+                            </a>
+                        </li>
+                        <li>
+                            <a href="09-class-abstract.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                               09 - Class Abstract
+                            </a>
+                        </li>
+                        <li>
+                            <a href="10-method-abstract.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                10 - Method Abstract
+                            </a>
+                        </li>
+                        <li>
+                            <a href="11-class-final.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                11 - Class Final
+                            </a>
+                        </li>
+                        <li>
+                            <a href="12-method-final.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                12 - Method Final
+                            </a>
+                        </li>
+                        <li>
+                            <a href="13-clone-object.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                13 - Clone Object
+                            </a>
+                        </li>
+                        <li>
+                            <a href="14-instance.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                14 - Instance
+                            </a>
+                        </li>
+                        <li>
+                            <a href="15-destruct.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                15 - Destruct
+                            </a>
+                        </li>
+                        <li>
+                            <a href="16-method-static.php" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                16 - Method Static
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                17 - Interface
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                18 - Name Space
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" class="bg-black 
+                                                bg-opacity-40 
+                                                w-96 p-4 
+                                                flex
+                                                text-xl
+                                              text-white/75 
+                                                rounded ">
+                                                
+                                                19 - Trait
+                            </a>
+                        </li>
+                    </ol>
+          
+        </section>
+
+    </main>
+    
+</body>
+</html>
